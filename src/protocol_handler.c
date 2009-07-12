6800a5a35e42cb99f0662115bab2b7c60a017722
Matthias Fassl <mf@x1598.at>
2009-07-12 19:47:46 +0200

    check if mail addresses contain at sign

--- src/protocol_handler.c
+++ src/protocol_handler.c
@@ -187,8 +187,6 @@ smtp_mail(const int socket,char* line) {
 char*
 smtp_rcpt(const int socket,const char* from,int authenticated, char* line) {
 	char *rcpt, *tmp;
-	//check if recipient is well formed
-	//TODO: check for at sign etc - external function check_mail_address() ?
 	if((tmp = extract_address(line)) == NULL) {
 		tcp_send_str(socket, SMTP_UNEXPECTED_MAIL_FORMAT);
 		return NULL;
@@ -199,6 +197,11 @@ smtp_rcpt(const int socket,const char* from,int authenticated, char* line) {
 	if(from == NULL) {
 		tcp_send_str(socket, SMTP_OUT_OF_SEQUENCE);
 		return rcpt;
+	}
+
+	//TODO: well formed? check for at sign etc - external function check_mail_address() ?
+	if(strchr(rcpt,'@') == NULL) {
+		return NULL;
 	}
 
 	char* domain = strchr(rcpt,'@')+1;
