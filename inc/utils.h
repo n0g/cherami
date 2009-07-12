0dfa8268895a7ef9dbe63de5ad2a3f44e287cf65
Matthias Fassl <matthias@jennifer.(none)>
2009-07-12 09:59:14 +0200

    added str_replace and str_toupper functions

--- inc/utils.h
+++ inc/utils.h
@@ -1,4 +1,6 @@
 void daemonize();
 char* stripCRLF(char* line);
 char* getpeeraddress(int socket);
-char* base64_decode(char* string);
+char* base64_decode(const char* string);
+char* str_replace(const char* string, char dst, char src);
+char* str_toupper(const char* string);
